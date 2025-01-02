1f8d969f55981a87421b5b0555c4069c8123d186
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-01 22:37:02 -0500

    Added author's note to main.cpp

--- DNN_/main.cpp
+++ DNN_/main.cpp
@@ -1,3 +1,18 @@
+/*
+Author: Daniel S. Jeremiah
+Date: January 1, 2025 
+
+Description:
+This file contains the propagation functions for the neural network. 
+Note that not all possible functions have been defined here. 
+Additional functions will be included as the project scales.
+
+Instructions:
+- Do not change the function definitions as they are expected to remain constant.
+- Ensure to follow the project guidelines for adding new functions.
+
+*/
+
 #include "./N_Network_/NET_CONSTRUCT/net_construct.h"
 #include <iostream>
 
