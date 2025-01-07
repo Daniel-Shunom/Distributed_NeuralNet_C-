e5f86036e2148fc06a69bfc6b060551b01ccd9c6
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-07 00:56:50 -0500

    Added author comments and default tags.

---
+++ DNN_Server_Components/Server.h
@@ -0,0 +1,19 @@
+/*
+Author: Daniel S. Jeremiah
+Date: January 6, 2024
+
+Description:
+CONTAINS SERVER HEADER FILES AND CLASSES
+
+Instructions:
+- Do not change the function definitions as they are expected to remain constant.
+- Ensure to follow the project guidelines for adding new functions.
+
+*/
+
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <ostream>
