af52846a3009435d87d9f6c78cfc2a1e959cecfb
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-06 20:53:16 -0500

    Refactored Code to use unscoped Matrix type def.
    Added matrix_appply method.

--- DNN/Data_Operations/MatrixOps.h
+++ DNN/Data_Operations/MatrixOps.h
@@ -9,14 +9,15 @@ class MatrixOps
 private:
     /* data */
 public:
-    DataOps::Matrix* matrix_add();
-    DataOps::Matrix* matrix_subtract();
-    DataOps::Matrix* matrix_multiply();
-    DataOps::Matrix* matrix_divide();
-    DataOps::Matrix* matrix_scale();
-    DataOps::Matrix* matrix_dot();
-    DataOps::Matrix* matrix_transpose();
-    DataOps::Matrix* matrix_add_scalar();
+    Matrix* matrix_add(Matrix* m1, Matrix* m2);
+    Matrix* matrix_subtract(Matrix* m1, Matrix* m2);
+    Matrix* matrix_multiply(Matrix* m1, Matrix* m2);
+    Matrix* matrix_divide(Matrix* m1, Matrix* m2);
+    Matrix* matrix_scale(double n, Matrix* m);
+    Matrix* matrix_dot(Matrix* m1, Matrix* m2);
+    Matrix* matrix_apply(double (*func)(double), Matrix* m);
+    Matrix* matrix_transpose(Matrix m1, Matrix m2);
+    Matrix* matrix_add_scalar(double n, Matrix* m);
 
     MatrixOps(/* args */);
     ~MatrixOps();
