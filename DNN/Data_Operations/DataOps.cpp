b168f051ba4a645559cabf93284d647c2e81d201
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-08 16:08:31 -0500

    added vector methods for better integration with existing architecture

--- DNN/Data_Operations/DataOps.cpp
+++ DNN/Data_Operations/DataOps.cpp
@@ -23,11 +23,28 @@ Matrix* DataOps::matrix_create(int row, int cols) {
     for (int i = 0; i < row; i++) {
         matrix->entries[i] = new double[cols];
     }
-    
+      
+    return matrix;
+}
+
+vMatrix* DataOps::v_matrix_create(int row, int cols) {
+    vMatrix *matrix = new vMatrix;
+    matrix->rows = row;
+    matrix->cols = cols;
+    matrix->entries.resize(row, std::vector<double>(cols));
+      
     return matrix;
 }
 
 void DataOps::matrix_fill(Matrix* m, int n) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->entries[i][j] = n;
+        }
+    }
+}
+
+void DataOps::v_matrix_fill(vMatrix* m, int n) {
     for (int i = 0; i < m->rows; i++) {
         for (int j = 0; j < m->cols; j++) {
             m->entries[i][j] = n;
@@ -44,7 +61,26 @@ void DataOps::matrix_free(Matrix* m) {
     m = NULL;
 }
 
+void DataOps::v_matrix_free(vMatrix *m) {
+    for (int i = 0; i < m->rows; i++) {
+        delete &m->entries[i];
+    }
+    delete &m->entries;
+    delete m;
+    m = NULL;
+}
+
 void DataOps::matrix_print(Matrix* m) {
+    std::cout << "[ROWS] " << m->rows << "[COLUMNS] " << m->cols << std::endl;
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            std::cout << std::fixed << std::setprecision(3) << m->entries[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void DataOps::v_matrix_print(vMatrix* m) {
     std::cout << "[ROWS] " << m->rows << "[COLUMNS] " << m->cols << std::endl;
     for (int i = 0; i < m->rows; i++) {
         for (int j = 0; j < m->cols; j++) {
@@ -65,7 +101,38 @@ Matrix* DataOps::matrix_copy(Matrix* m) {
     return mat;
 }
 
+vMatrix* DataOps::v_matrix_copy(vMatrix* m) {
+    vMatrix* mat = v_matrix_create(m->rows, m->cols);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            mat->entries[i][j] = m->entries[i][j];
+        }
+    }
+
+    return mat;
+}
+
 void DataOps::matrix_save(Matrix* m, char* file_string) {
+    std::ofstream file(file_string); 
+    if (!file) { 
+        std::cerr << "Error opening file: " << file_string << std::endl; 
+        return; 
+    } 
+
+    file << m->rows << std::endl; 
+    file << m->cols << std::endl; 
+
+    for (int i = 0; i < m->rows; ++i) { 
+        for (int j = 0; j < m->cols; ++j) { 
+            file << std::fixed << std::setprecision(6) << m->entries[i][j] << std::endl; 
+        } 
+    } 
+
+    std::cout << "Successfully saved matrix to " << file_string << std::endl; 
+    file.close();
+}
+
+void DataOps::v_matrix_save(vMatrix* m, char* file_string) {
     std::ofstream file(file_string); 
     if (!file) { 
         std::cerr << "Error opening file: " << file_string << std::endl; 
@@ -106,6 +173,27 @@ Matrix* DataOps::matrix_load(char *file_string) {
     return m;
 }
 
+vMatrix* DataOps::v_matrix_load(char *file_string) {
+    std::ifstream file(file_string);
+    if (!file) {
+        std::cerr << "Error opening file: " << file_string << std::endl;
+        return nullptr;
+    }
+
+    int rows, cols;
+    file >> rows >> cols;
+
+    vMatrix* m = v_matrix_create(rows, cols);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            file >> m->entries[i][j];
+        }
+    }
+
+    std::cout << "LOADED MATRIX FROM" << file_string << std::endl;
+    return m;
+}
+
 double DataOps::uniform_distribution(double low, double high) {
     double difference = high - low;
     int scale = 10000;
@@ -115,6 +203,16 @@ double DataOps::uniform_distribution(double low, double high) {
 }
 
 void DataOps::matrix_randomize(Matrix* m, int n) {
+    double min = -1.0/sqrt(n);
+    double max = 1.0/sqrt(n);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->entries[i][j] = uniform_distribution(min, max);
+        }
+    }
+}
+
+void DataOps::v_matrix_randomize(vMatrix* m, int n) {
     double min = -1.0/sqrt(n);
     double max = 1.0/sqrt(n);
     for (int i = 0; i < m->rows; i++) {
@@ -137,6 +235,19 @@ int DataOps::matrix_argmax (Matrix* m) {
     return max_idx;
 }
 
+int DataOps::matrix_argmax (vMatrix* m) {
+    double max_score = 0;
+    int max_idx = 0;
+    for (int i = 0; i < m->rows; i++) {
+        if (m->entries[i][0] > max_score) {
+            max_score = m->entries[i][0];
+            max_idx = i;
+        }
+    }
+
+    return max_idx;
+}
+
 Matrix* DataOps::matrix_flatten(Matrix* m, int axis) { 
     // Axis = 0 -> Column Vector, Axis = 1 -> Row Vector 
     Matrix* mat; if (axis == 0) { 
@@ -144,6 +255,33 @@ Matrix* DataOps::matrix_flatten(Matrix* m, int axis) {
     } 
     else if (axis == 1) { 
         mat = matrix_create(1, m->rows * m->cols); 
+    } 
+    else { 
+        std::cerr << "Argument to matrix_flatten must be 0 or 1" << std::endl; 
+        exit(EXIT_FAILURE); 
+    } 
+    
+    for (int i = 0; i < m->rows; ++i) { 
+        for (int j = 0; j < m->cols; ++j) { 
+            if (axis == 0) { 
+                mat->entries[i * m->cols + j][0] = m->entries[i][j];
+            } 
+            else if (axis == 1) { 
+                mat->entries[0][i * m->cols + j] = m->entries[i][j];
+            } 
+        } 
+    }
+
+    return mat;
+}
+
+vMatrix* DataOps::v_matrix_flatten(vMatrix* m, int axis) { 
+    // Axis = 0 -> Column Vector, Axis = 1 -> Row Vector 
+    vMatrix* mat; if (axis == 0) { 
+        mat = v_matrix_create(m->rows * m->cols, 1); 
+    } 
+    else if (axis == 1) { 
+        mat = v_matrix_create(1, m->rows * m->cols); 
     } 
     else { 
         std::cerr << "Argument to matrix_flatten must be 0 or 1" << std::endl; 
