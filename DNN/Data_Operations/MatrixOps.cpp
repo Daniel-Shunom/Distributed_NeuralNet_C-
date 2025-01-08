82e219deaf24ae75c1bdee56c1265ff5c52c76af
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-08 17:10:39 -0500

    ddefined vector function types using vMatrix

--- DNN/Data_Operations/MatrixOps.cpp
+++ DNN/Data_Operations/MatrixOps.cpp
@@ -37,6 +37,24 @@ Matrix* MatrixOps::matrix_add(Matrix* m1, Matrix* m2) {
     return m;
 }
 
+vMatrix* MatrixOps::v_matrix_add(vMatrix* m1, vMatrix* m2) {
+    
+    if (!checkDimensions(m1, m2)) {
+        throw std::invalid_argument(
+            "Dimesion mismatch: "
+        );
+    }
+
+    vMatrix* m =  v_matrix_create(m1->rows, m1->cols);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->entries[i][j] = m1->entries[i][j] + m2->entries[i][j];
+        }
+    }
+
+    return m;
+}
+
 Matrix* MatrixOps::matrix_subtract(Matrix* m1, Matrix* m2) {
     
     if (!checkDimensions(m1, m2)) {
@@ -55,6 +73,24 @@ Matrix* MatrixOps::matrix_subtract(Matrix* m1, Matrix* m2) {
     return m;
 }
 
+vMatrix* MatrixOps::v_matrix_subtract(vMatrix* m1, vMatrix* m2) {
+    
+    if (!checkDimensions(m1, m2)) {
+        throw std::invalid_argument(
+            "Dimesion mismatch: "
+        );
+    }
+
+    vMatrix* m =  v_matrix_create(m1->rows, m1->cols);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->entries[i][j] = m1->entries[i][j] - m2->entries[i][j];
+        }
+    }
+
+    return m;
+}
+
 Matrix* MatrixOps::matrix_multiply(Matrix* m1, Matrix* m2) {
     
     if (!checkDimensions(m1, m2)) {
@@ -64,6 +100,24 @@ Matrix* MatrixOps::matrix_multiply(Matrix* m1, Matrix* m2) {
     }
 
     Matrix* m =  matrix_create(m1->rows, m1->cols);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            m->entries[i][j] = m1->entries[i][j] * m2->entries[i][j];
+        }
+    }
+
+    return m;
+}
+
+vMatrix* MatrixOps::v_matrix_multiply(vMatrix* m1, vMatrix* m2) {
+    
+    if (!checkDimensions(m1, m2)) {
+        throw std::invalid_argument(
+            "Dimesion mismatch: "
+        );
+    }
+
+    vMatrix* m =  v_matrix_create(m1->rows, m1->cols);
     for (int i = 0; i < m->rows; i++) {
         for (int j = 0; j < m->cols; j++) {
             m->entries[i][j] = m1->entries[i][j] * m2->entries[i][j];
@@ -95,8 +149,40 @@ Matrix* MatrixOps::matrix_dot(Matrix* m1, Matrix* m2) {
     return m;
 }
 
+vMatrix* MatrixOps::v_matrix_dot(vMatrix* m1, vMatrix* m2) {
+    
+    if (!checkDimensions(m1, m2)) {
+        throw std::invalid_argument(
+            "Dimesion mismatch: "
+        );
+    }
+    vMatrix* m = v_matrix_create(m1->rows, m1->cols);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            double sum = 0;
+            for (int k = 0; k < m2->rows; m++) {
+                sum += m1->entries[i][j] * m2->entries[i][j];
+            }
+            m->entries[i][j] =sum;
+        }
+    }
+
+    return m;
+}
+
 Matrix* MatrixOps::matrix_scale(double n, Matrix* m) {
     Matrix* mat = matrix_copy(m);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            mat->entries[i][j] *= n;
+        }
+    }
+
+    return mat;
+}
+
+vMatrix* MatrixOps::v_matrix_scale(double n, vMatrix* m) {
+    vMatrix* mat = v_matrix_copy(m);
     for (int i = 0; i < m->rows; i++) {
         for (int j = 0; j < m->cols; j++) {
             mat->entries[i][j] *= n;
@@ -117,6 +203,17 @@ Matrix* MatrixOps::matrix_add_scalar(double n, Matrix* m) {
     return mat;
 }
 
+vMatrix* MatrixOps::v_matrix_add_scalar(double n, vMatrix* m) {
+    vMatrix* mat = v_matrix_copy(m);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            mat->entries[i][j] += n;
+        }
+    }
+
+    return mat;
+}
+
 Matrix* MatrixOps::matrix_apply(double (*func)(double), Matrix* m) {
     Matrix *mat = matrix_copy(m);
 	for (int i = 0; i < m->rows; i++) {
@@ -128,8 +225,30 @@ Matrix* MatrixOps::matrix_apply(double (*func)(double), Matrix* m) {
 	return mat;
 }
 
+vMatrix* MatrixOps::v_matrix_apply(double (*func)(double), vMatrix* m) {
+    vMatrix *mat = v_matrix_copy(m);
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			mat->entries[i][j] = (*func)(m->entries[i][j]);
+		}
+	}
+    
+	return mat;
+}
+
 Matrix* MatrixOps::matrix_transpose(Matrix* m) {
     Matrix* mat = matrix_copy(m);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            mat->entries[i][j] = m->entries[i][j];
+        }
+    }
+
+    return mat;
+}
+
+vMatrix* MatrixOps::v_matrix_transpose(vMatrix* m) {
+    vMatrix* mat = v_matrix_copy(m);
     for (int i = 0; i < m->rows; i++) {
         for (int j = 0; j < m->cols; j++) {
             mat->entries[i][j] = m->entries[i][j];
