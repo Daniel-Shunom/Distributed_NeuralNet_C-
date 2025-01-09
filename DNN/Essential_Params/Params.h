1b47098fbb46cd403b3d6ecd2957d173e687a087
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-08 21:50:58 -0500

    added author notes.

--- DNN/Essential_Params/Params.h
+++ DNN/Essential_Params/Params.h
@@ -1,3 +1,18 @@
+/*
+Author: Daniel S. Jeremiah
+Date: January 8, 2024
+
+Description:
+This file contains the aliased for the parameters of the neural 
+network. Note that not all possible functions have been defined here. 
+Additional functions will be included as the project scales.
+
+Instructions:
+- Do not change the function definitions as they are expected to remain constant.
+- Ensure to follow the project guidelines for adding new functions.
+
+*/
+
 #pragma once
 
 #include <vector>
