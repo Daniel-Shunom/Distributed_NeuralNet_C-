748878ddc24f205ae03b643c2fc6f5d193a05e4b
Daniel-Shunom <141185537+Daniel-Shunom@users.noreply.github.com>
2025-01-12 03:44:33 -0500

    tested file handling methods

--- DNN/main.cpp
+++ DNN/main.cpp
@@ -15,49 +15,30 @@ Instructions:
 
 #include "./NET_CONSTRUCT/net_construct.h"
 #include "./Data_Operations/MatrixOps.h"
+#include "./Image_Operations/ImgOps.h"
 #include <iostream>
+//#include "../F_MNIST/fashion-mnist_test.csv"
 
 int main() {
     //MenuConstruct m;
     //m.menu_configuration();
     MatrixOps mp;
+    Image img;
     vMatrix* m1 = mp.v_matrix_create(10, 10);
     vMatrix* m2 = mp.v_matrix_create(10, 10);
 
     vMatrix* m3 = mp.v_matrix_create(4, 2);
     vMatrix* m4 = mp.v_matrix_create(2, 4);
 
-    //m1->entries[0][0] = 1;
-    //mp.v_matrix_fill(m1, rand()/RAND_MAX);
-    //mp.v_matrix_print(m1);
-    //std::cout << "\n\n";
-//
-    //m2->entries[0][0] = 1;
-    //mp.v_matrix_fill(m2, rand()/RAND_MAX);
-    //mp.v_matrix_print(m2);
-    //std::cout << "\n\n";
-
-    m3->entries[0][0] = 1;
-    mp.v_matrix_fill(m3, rand()/RAND_MAX);
-    mp.v_matrix_print(m3);
-    std::cout << "\n\n";
-
-    m4->entries[0][0] = 1;
-    mp.v_matrix_fill(m4, rand()/RAND_MAX);
-    mp.v_matrix_print(m4);
-    std::cout << "\n\n";
-
-    mp.v_matrix_randomize(m3, 3);
-    mp.v_matrix_print(m3);
-    std::cout << "\n\n";
-
-    mp.v_matrix_randomize(m4, 3);
-    mp.v_matrix_print(m4);
-    std::cout << "\n\n";
-
-
-    vMatrix* m5 = mp.v_matrix_create(m1->rows, m2->cols);
-    m5 = mp.v_matrix_dot(m3, m4);
-    mp.v_matrix_print(m5);
+    int val = 2;
+    Img** image = img.csv_to_imgs(
+        "C:/Users/danie/OneDrive/Desktop/C++/F_MNIST/fashion-mnist_test.csv", 
+        val
+    );
+    
+    for (int i = 0; i < val; i++) {
+        img.img_print(image[i]);
+    }
+    //mp.v_matrix_print(image[0]->img_data);
     return 0;
 }
