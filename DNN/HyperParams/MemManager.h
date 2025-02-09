8b21e15109cb81c8b1ef5e2bacd02bad84f040b8
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-02-08 22:08:09 -0500

    used pointer reference.

--- DNN/HyperParams/MemManager.h
+++ DNN/HyperParams/MemManager.h
@@ -12,7 +12,7 @@ private:
     static std::vector<std::shared_ptr<vMatrix>> MemCache;
 public:
     static MemManager& getInstance();
-    void ptr_freeze(std::shared_ptr<vMatrix> object1);
+    void ptr_freeze(std::shared_ptr<vMatrix> &object1);
     void ptr_release();
     void mem_size();
 };
