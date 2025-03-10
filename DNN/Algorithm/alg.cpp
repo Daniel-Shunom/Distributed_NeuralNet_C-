6d9e20d54245053dbe61e997abb41b3ce4916b84
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-10 00:22:51 -0400

    forward pass implementations

---
+++ DNN/Algorithm/alg.cpp
@@ -0,0 +1,35 @@
+#include "./alg.h"
+
+void Alg::set_input_lyr(LC &cache, vMatrix mx) {
+    std::cout<< "setting input layer\n";
+    for (auto &nd: cache[0]) {
+        *nd.inputs = mx;
+    }
+    std::cout<< "set layer\n";
+}
+
+std::shared_ptr<vMatrix> Alg::calc_prod(MatrixOps m, Node v) {
+    std::shared_ptr<vMatrix> temp = m.v_matrix_multiply(v.inputs, v.Weights);
+    return m.v_matrix_add(temp, v.Biases);
+}
+
+void Alg::forwardprop(LC &cache) {
+    std::cout<< "pass called \n";
+    Propagation p;
+    MatrixOps m;
+    std::shared_ptr<vMatrix> mx;
+    size_t csh = cache.size();
+    int idx = 0;
+    for (auto &lyr: cache) {
+        idx++;
+        std::shared_ptr<vMatrix> sum;
+        size_t l = lyr.size();
+        for (size_t i = 0; i<l-1; i++) {
+            mx = calc_prod(m, lyr[i]);
+            lyr[i].Activations = p.v_tanh_activation(mx);
+            sum = m.v_matrix_add(sum, lyr[i].Activations);
+            *lyr[idx+1].inputs = *sum;
+        }
+        NetCache.insert(std::make_pair(idx, *sum));
+    }
+}
---
+++ DNN/Algorithm/alg.h
@@ -0,0 +1,16 @@
+#pragma once 
+#include <unordered_map>
+#include "../Essential_Params/Params.h"
+#include "../Essential_Methods/Data_Operations/MatrixOps.h"
+#include "../Essential_Methods/Propagations/Propagation.h"
+
+
+class Alg {
+public:
+    std::unordered_map<int, vMatrix> NetCache = {};
+    std::shared_ptr<vMatrix> calc_prod(MatrixOps m, Node v);
+    void set_input_lyr(LC &cache, vMatrix mx);
+    //void backprop();
+    void forwardprop(LC &cache);
+    //void param_cache();
+};
