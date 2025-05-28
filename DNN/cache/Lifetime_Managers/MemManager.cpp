5ed117dfa4c27c7d0448b37fc9bd454466cd91c8
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-05-28 16:10:06 -0400

    minor refactor

--- DNN/cache/Lifetime_Managers/MemManager.cpp
+++ DNN/cache/Lifetime_Managers/MemManager.cpp
@@ -4,18 +4,19 @@
 MemManager MemManager::my_instance;
 std::vector<std::shared_ptr<vMatrix>> MemManager::MemCache;
 
-MemManager& MemManager::getInstance() {
-    return my_instance;
+void MemManager::mem_size() {
+    std::cout << "Memory Cache Size: " << MemCache.size() << '\n';
 }
 
 void MemManager::ptr_freeze(std::shared_ptr<vMatrix> &object1) {
     MemCache.push_back(object1);
 }
 
+MemManager& MemManager::getInstance() {
+    return my_instance;
+}
+
 void MemManager::ptr_release() {
     MemCache.clear();
 }
 
-void MemManager::mem_size() {
-    std::cout << "Memory Cache Size: " << MemCache.size() << '\n';
-}
